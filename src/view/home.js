8f412b9274eba98b9043cf78c30b3e05516fb4ba
BrigitteNikoll <BrigitteNikoll@users.noreply.github.com>
2021-05-04 12:58:56 -0500

    funciónes default

--- src/view/home.js
+++ src/view/home.js
@@ -1,7 +1,22 @@
 export default () => {
   const viewHome = document.createElement('div');
   viewHome.innerHTML = `
-      <header>murito</header>
+      <div class="containerPost">
+        <div class="boxPost">
+          <form action="">
+            <textarea id="formPost" placeholder="¿Qué estás pensando?" spellcheck = "false" required></textarea>
+          </form> 
+        </div>
+      </div>
+
+      <div class="optionSeccion">
+        <select id="privacy-option">
+          <option value="public" title = "Public">Public</option>
+          <option value="private" title = "Private">Private</option>
+        </select>
+        <button type="submit" id="btnPost" >Publicar</button>
+      </div>
+      
       
        `;
 
--- src/view/profile.js
+++ src/view/profile.js
@@ -1,7 +1,8 @@
 export default () => {
   const viewProfile = document.createElement('div');
   viewProfile.innerHTML = `
-        <header>actualizacion de perfil </header>`;
-
+        <header>actualizacion de perfil </header>
+        
+      `;
   return viewProfile;
 };
