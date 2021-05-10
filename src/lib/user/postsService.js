31ac08d3b6e8fcb05505131bdadbf9c1ce0adf13
BrigitteNikoll <BrigitteNikoll@users.noreply.github.com>
2021-05-09 19:40:28 -0500

    FIX: Trying to delete posts

--- src/lib/user/postsService.js
+++ src/lib/user/postsService.js
@@ -19,73 +19,15 @@ export const getPost = (callback) => {
     querySnapshot.forEach((doc) => {
       post.push({
         postUs: doc.data().posting,
+        // id: doc.data().id,
       });
     });
+
     console.log(post);
     callback(post);
   });
+  /*  firebase.firestore().collection('posts').get().then(e=>{
+    e (doc => {
+      firebase.firestore().collection('posts').doc(doc.id).delete()
+    })}); */
 };
-
-// --------FIRESTORE---------------
-
-// export const deletePost = (id) => {
-//   const db = firebase.firestore();
-//   return db.collection('posts').doc(id).delete();
-// };
-
-// var db = firebase.firestore();
-// function guardar() {
-//   var saveInformation = document.getElementById('formPost').value;
-
-//   db.collection("users").add({
-//     posting: saveInformation
-//   })
-//   .then((docRef) => {
-
-//     consoe.log("Document written with ID: ", docRef.id);
-//     document.getElementById('formPost').value = '';
-//   })
-//   .catch((error) => {
-//     console.error("Error adding document: ", error);
-//   });
-
-// }
-
-// const divSeccion = document.getElementById('boxPostShare');
-// db.collection('users').onSnapshot((querySnapshot) => {
-//   divSeccion.innerHTML = '';
-//   querySnapshot.forEach((document) => {
-//     console.log(`${document.id} => ${document.data().posting}`);
-//     divSeccion.innerHTML += `
-//       <div id="boxPostShare">
-//         <form action="">
-//           <textarea id="formPostShare" spellcheck = "false" required>${document.data().posting}</textarea>
-//         </form>
-//       </div>`;
-//   });
-// });
-
-// Leer documentos
-
-/* const divSeccion = document.getElementById('boxPostShare')
-db.collection("users").get().then((querySnapshot) => {
-  divSeccion.innerHTML = '';
-  querySnapshot.forEach((document) => {
-      console.log(`${document.id} => ${document.data().posting}`);
-      divSeccion.innerHTML += `
-      <div id="boxPostShare">
-        <form action="">
-          <textarea id="formPostShare" spellcheck = "false" required>${document.data().posting}</textarea>
-        </form>
-      </div>`
-
-  });
-}); */
-
-/* export const deletePost = () => {
-  firebase.firestore().collection('posts').get().then(querySnapshot => {
-    querySnapshot.forEach(doc => {
-    console.log(firebase.firestore().collection('posts').doc(doc.id).delete())
-  })
-});
-} */
--- src/view/posts.js
+++ src/view/posts.js
@@ -1,31 +1,32 @@
-import { getPost /* ,deletePost */ } from '../lib/user/postsService.js';
+import { getPost, /* deletePost */ } from '../lib/user/postsService.js';
 
 export const renderPostUser = (element) => {
   const postUser = document.createElement('div');
 
   getPost((postUser1) => {
-    // console.log(postUser1);
+    // console.log('postUser1:', postUser1[0].id);
     postUser1.forEach((doc) => {
-      // postUser.innerHTML = '';
+      postUser.innerHTML = '';
       const postUnique = document.createElement('div');
       postUnique.innerHTML = `
             <textarea id="formPostShare" spellcheck = "false" required>${doc.postUs}</textarea>
-            <button id="btnPostEdit" >edit</button>
-            <button id="btnPostDelete">delete</button>
+            <button id="btnPostEdit">edit</button>
+            <button id="btn-delete" >delete</button>
             `;
       postUser.appendChild(postUnique);
     });
-    postUser.querySelectorAll('#btnPostDelete').forEach((formPostDelete) => formPostDelete.addEventListener('click', (e) => {
-      console.log(e.target.tagName);
-      console.log(e.target.parentElement.getAttribute());
-      console.log('me borro');
-      //deletePost();
-    }));
-
-    postUser.querySelectorAll('#btnPostEdit').forEach((formPostEdit) => formPostEdit.addEventListener('click', () => console.log('me edito')));
-
-    console.log(postUser);
-
     element.appendChild(postUser);
   });
 };
+
+
+    /*   //Borrar post
+    postUser.querySelectorAll('#btnPostDelete').forEach((formPostDelete) => formPostDelete.addEventListener('click', () => {
+      //console.log(e.target.tagName);
+      //console.log(e.target.parentElement.getAttribute('idfire'));
+      //console.log('idfire', formPostDelete.getAttribute('idfire'));
+
+      //deletePost(formPostDelete.getAttribute('idfire'));
+      console.log(deletePost(formPostDelete.getAttribute('idfire')))
+
+    })); */
